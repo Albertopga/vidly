de1eaa05707e75bc368d19a835c4103e4665818e
Alberto Perez <alberto.perez@cloudappi.net>
2022-10-13 10:17:47 +0200

    impruvments

--- routes/returns.js
+++ routes/returns.js
@@ -9,10 +9,7 @@ router.post('/', auth, async (req, res) => {
   if(!req.body.customerId) return res.status(400).send('customerId not provider')
   if(!req.body.movieId) return res.status(400).send('movieId not provider')
 
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId,
-    'movie._id': req.body.movieId
-  })
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId)
 
   if(!rental) return res.status(404).send('No rental found for this customer/movie')
   if(rental.dateReturned) return res.status(400).send('Return already processed')
